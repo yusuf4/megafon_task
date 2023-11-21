58b3e4914140fcdb0a296d53560d72467cffc888
unknown <ysafarov8@gmail.com>
2023-11-21 12:54:16 +0300

    configured enveroment

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex">
+       <div class="bg-red-400">
+           COL 1
+       </div>
+        <div class="text-blue-500 font-bold">
+            COL 2
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Test"
+}
+</script>
